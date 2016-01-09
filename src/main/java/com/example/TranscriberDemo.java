3e8668cea9cc6bf8d138010b65c70582ed509846
akilamunasinghe <akilamunasinghe@gmail.com>
2016-01-09 22:02:54 +0530

    recognizing words

--- src/main/java/com/example/TranscriberDemo.java
+++ src/main/java/com/example/TranscriberDemo.java
@@ -37,7 +37,6 @@ public class TranscriberDemo {
         SpeechResult result;
         // Pause recognition process. It can be resumed then with startRecognition(false).
         do{
-       //     result=n
             result = recognizer.getResult();
             //System.out.format("Hypothesis: %s\n", result.getHypothesis());
             str=result.getHypothesis();
